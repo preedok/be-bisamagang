e280fe749c4b366dfc2b44e4c1a9604080f6b874
sukacode <firmansahalhadi@gmail.com>
2025-01-08 11:13:18 +0700

    feat: add users models crud

--- models/users.models.js
+++ models/users.models.js
@@ -5,4 +5,42 @@ const getAllUser = async () => {
   return users.rows;
 };
 
-module.exports = { getAllUser };
+const getUserByUsername = async (username) => {
+  const user = await pool.query("SELECT * FROM users WHERE username = $1", [
+    username,
+  ]);
+  return user.rows[0];
+};
+
+const addUser = async (user) => {
+  try {
+    const newUser = await pool.query(
+      "INSERT INTO users (username, email, password, fullname, role) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [
+        user.username,
+        user.email,
+        user.password,
+        user.fullname,
+        user.role || "user",
+      ]
+    );
+
+    if (newUser.rows.length === 0) {
+      throw new Error("Gagal menambahkan user");
+    }
+
+    return newUser.rows[0];
+  } catch (error) {
+    throw new Error(`Error saat menambahkan user: ${error.message}`);
+  }
+};
+
+const deleteUser = async (username) => {
+  const deletedUser = await pool.query(
+    "DELETE FROM users WHERE username = $1",
+    [username]
+  );
+  return deletedUser.rows[0];
+};
+
+module.exports = { getAllUser, getUserByUsername, addUser, deleteUser };
