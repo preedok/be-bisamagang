26e8677ff658a56f2509a0cf00662710caf17a28
sukacode <firmansahalhadi@gmail.com>
2025-01-08 11:16:17 +0700

    feat: update user routes for crud

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 
 router.get("/users-list", userController.getUsers);
+router.post("/add-user", userController.addUser);
+router.get("/users/:username", userController.getUserByUsername);
+router.delete("/users/:username", userController.deleteUser);
 
 module.exports = router;
