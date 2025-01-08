12e0591e679639ee33c23f58d157d3fc43036beb
sukacode <firmansahalhadi@gmail.com>
2025-01-08 12:39:04 +0700

    fix(routes): update getUserByUsername

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,7 +3,7 @@ const userController = require("../controllers/user.controller");
 
 router.get("/users-list", userController.getUsers);
 router.post("/add-user", userController.addUser);
-router.get("/users/:username", userController.getUserByUsername);
+router.get("/user/:username", userController.getUserByUsername);
 router.delete("/users/:username", userController.deleteUser);
 
 module.exports = router;
